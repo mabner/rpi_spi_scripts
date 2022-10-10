a6f57b7c4a1f2c04233f6994e5e2fbe9d0484dcb
Marcos Leite <m.abner@live.co.uk>
2022-10-10 12:23:41 -0300

    showing external ip address using STUN server with pynat

--- spi_show_hostname_ip.py
+++ spi_show_hostname_ip.py
@@ -3,13 +3,20 @@ from luma.core.interface.serial import spi
 from luma.core.render import canvas
 from luma.oled.device import ssd1306
 import socket
+from pynat import get_ip_info
 
 serial = spi(device=0, port=0)
 device = ssd1306(serial)
 
+# Hostname
 hostname = socket.gethostname()
+
+# Internal IP
 ip = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 ip.connect(("0.0.0.0", 80))
+
+# External IP
+topology, ext_ip, ext_port = get_ip_info()
 
 while True:
     print(hostname)
@@ -20,3 +27,5 @@ while True:
         draw.text((5, 5), hostname, fill="white")
         draw.text((0, 10), "---------------------", fill="white")
         draw.text((5, 15), ip.getsockname()[0], fill="white")
+        draw.text((0, 20), "---------------------", fill="white")
+        draw.text((5, 25), ext_ip, fill="white")
