3d974a96d0cac7aa01098e31b5e14f589bc7ce0a
Marcos Leite <m.abner@live.co.uk>
2022-10-10 05:10:02 -0300

    input for display time

--- spi_input_text.py
+++ spi_input_text.py
@@ -1,17 +1,17 @@
-from luma.core.interface.serial import i2c, spi, pcf8574
-from luma.core.interface.parallel import bitbang_6800
+from luma.core.interface.serial import spi
 from luma.core.render import canvas
-from luma.oled.device import ssd1306, ssd1309, ssd1325, ssd1331, sh1106, ws0010
+from luma.oled.device import ssd1306
+import datetime
 
 serial = spi(device=0, port=0)
-
 device = ssd1306(serial)
-repeat = 1
 
-text =  input("Text to display:")
+text = input("Text to display: ")
+delay = int(input("Run for X seconds: "))
+time = datetime.datetime.now() + datetime.timedelta(seconds=delay)
 
-while repeat == 1:
-	with canvas(device) as draw:
-    		draw.rectangle(device.bounding_box, outline="white", fill="black")
-    		draw.text((30, 40), text, fill="white")
-
+while datetime.datetime.now() < time:
+    with canvas(device) as draw:
+        draw.rectangle(device.bounding_box, outline="white", fill="black")
+        draw.text((58, 5), text, fill="white")
+        draw.text((32, 10), "............", fill="white")
